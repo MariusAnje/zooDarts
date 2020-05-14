4c44de5f9eea5bd1479ca039e06fd18087275a9c
weiwenjiang <jiang.wwen@gmail.com>
2020-05-14 10:49:00 +0800

    this doesnt work kkk

--- simpleExp/simpleExp.py
+++ simpleExp/simpleExp.py
@@ -5,9 +5,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 # tqdm is imported for better visualization
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 
-def train(numEpoch):
+def train(numEpoch, device):
     """
         Typical training scheme for offline training.
     """
@@ -20,6 +20,7 @@ def train(numEpoch):
             for i, data in enumerate(loader, 0):
                 # get the inputs; data is a list of [inputs, labels]
                 inputs, labels = data
+                inputs, labels = inputs.to(device), labels.to(device)
 
                 # zero the parameter gradients
                 optimizer.zero_grad()
@@ -33,13 +34,13 @@ def train(numEpoch):
                 # print statistics
                 running_loss += loss.item()
                 loader.set_description(f"{running_loss/(i+1):.4f}")
-        acc = test()
+        acc = test(device)
         print(f"Epoch {epoch}: test accuracy: {acc:.4f}")
         if acc > best_Acc:
             best_Acc = acc
-            torch.save(net.state_dict(), './MNIST.pt')
+            torch.save(net.state_dict(), './CIFAR10.pt')
 
-def test():
+def test(device):
     """
         Typical inference scheme for both offline validation and online inference.
     """
@@ -49,35 +50,13 @@ def test():
         net.eval()
         for data in testloader:
             images, labels = data
+            images, labels = images.to(device), labels.to(device)
             outputs = net(images)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
     return correct/total
-
-
-   
-# Determining the use scheme
-offline = True
-
-# Hyper parameters for training offline and inference online
-if offline:
-    batchSize = 32
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-else:
-    batchSize = 8
-    device = torch.device("cpu")
-
-# dataset extracting and data preprocessing
-transform = transforms.Compose(
-[transforms.ToTensor(),
- transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-trainset = torchvision.datasets.CIFAR10(root='/dataset', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batchSize, shuffle=True, num_workers=4)
-testset = torchvision.datasets.CIFAR10(root='/dataset', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batchSize, shuffle=False, num_workers=4)
-
 
 class Block(nn.Module):
     # supported type: CONV1, CONV3, CONV5, CONV7, ID
@@ -156,31 +135,53 @@ class SuperNet(nn.Module):
         return x
 
 
-# model
-net = SuperNet()
+if __name__ == "__main__":
+    # Determining the use scheme
+    offline = True
 
-if offline:
-    # Offline training
+    # Hyper parameters for training offline and inference online
+    if offline:
+        batchSize = 64
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    else:
+        batchSize = 8
+        device = torch.device("cpu")
 
-    # loss function and optimizer
-    criterion = nn.CrossEntropyLoss()
-    # optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    # dataset extracting and data preprocessing
+    transform = transforms.Compose(
+    [transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    trainset = torchvision.datasets.CIFAR10(root='/dataset/CIFAR10', train=True, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batchSize, shuffle=True, num_workers=4)
+    testset = torchvision.datasets.CIFAR10(root='/dataset/CIFAR10', train=False, download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batchSize, shuffle=False, num_workers=4)
 
-    # Training
-    train(10)
+    # model
+    net = SuperNet()
+    net.to(device)
 
-    # Validation
-    state_dict = torch.load("./CIFAR10.pt")
-    net.load_state_dict(state_dict)
-    print(f"Test accuracy: {test()}")
+    if offline:
+        # Offline training
 
-else:
-    # Online inference
+        # loss function and optimizer
+        criterion = nn.CrossEntropyLoss()
+        # optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+        optimizer = optim.Adam(net.parameters(), lr=0.001)
 
-    # The pretrained model
-    state_dict = torch.load("./CIFAR10.pt")
-    net.load_state_dict(state_dict)
+        # Training
+        train(30, device)
 
-    # Actual inference
-    print(f"Test accuracy: {test()}")
+        # Validation
+        state_dict = torch.load("./CIFAR10.pt")
+        net.load_state_dict(state_dict)
+        print(f"Test accuracy: {test(device)}")
+
+    else:
+        # Online inference
+
+        # The pretrained model
+        state_dict = torch.load("./CIFAR10.pt")
+        net.load_state_dict(state_dict)
+
+        # Actual inference
+        print(f"Test accuracy: {test(device)}")
