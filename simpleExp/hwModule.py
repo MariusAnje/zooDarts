fbf8438c4d838724571e91a8200b287dc982d575
MariusAnje <mariusanje@outlook.com>
2020-05-15 14:44:23 -0400

    inference

--- simpleExp/hwModule.py
+++ simpleExp/hwModule.py
@@ -17,10 +17,17 @@ class HWMixedBlock(nn.Module):
     def forward(self, x):
         x = self.convBlock(x)
         p = 1 - self.sm(self.mix)
-        output = (x.transpose(1,3) * p).transpose(1,3)
+        output = (x.transpose(1,3) * p).transpose(1,3) * (float(len(p))/float((len(p) - 1)))
+        return output
+    
+    def inference(self, x)
+        x = self.convBlock(x)
+        p = self.mix / self.mix
+        p[self.mix.argmax()] *= 0
+        output = (x.transpose(1,3) * p).transpose(1,3) * (float(len(p))/float((len(p) - 1)))
         return output
         
-class HWnet(nn.Module):
+class HWnet_old(nn.Module):
     def __init__(self, num_classes = 10):
         super(HWnet, self).__init__()
         
@@ -43,5 +50,35 @@ class HWnet(nn.Module):
 
     def forward(self, x):
         x = self.feature(x)
+        x = self.classifier(x.view(-1, 512*4*4))
+        return x
+
+class HWnet(nn.Module):
+    def __init__(self, num_classes = 10):
+        super(HWnet, self).__init__()
+        
+        self.block1 = HWMixedBlock(3,128,True)
+        self.block2 = HWMixedBlock(128,128,True)
+        self.block3 = HWMixedBlock(128,256,True)
+        self.block4 = HWMixedBlock(256,256,True)
+        self.block5 = HWMixedBlock(256,512,True)
+        self.block6 = HWMixedBlock(512,512,True)
+        self.pool   = nn.MaxPool2d(2)
+        self.classifier = nn.Sequential(
+            nn.Linear(512*4*4, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.pool(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.pool(x)
+        x = self.block5(x)
+        x = self.block6(x)
+        x = self.pool(x)
         x = self.classifier(x.view(-1, 512*4*4))
         return x
