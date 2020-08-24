697dd02d0af8978d9ddc97c84712f42e24eae87b
MariusAnje <mariusanje@outlook.com>
2020-08-24 16:09:23 -0400

    optim

--- hyberDARTS/RL/backend.py
+++ hyberDARTS/RL/backend.py
@@ -11,6 +11,7 @@ def fit(model, optimizer=None, train_data=None, val_data=None, epochs=40,
     loss = []
     timer = Timer()
     timer.reset()
+    print(optimizer)
     for epoch in range(epochs):
         if lr_schedule is not None:
             lr_schedule(optimizer, epoch)
